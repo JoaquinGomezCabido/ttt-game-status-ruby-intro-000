47fbff3117e6dfb177a195cf5decd54b5f2e83dc
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-12 21:14:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,12 +17,17 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |array|
-    winning_cell_1 = array[0]
-    winning_cell_2 = array[1]
-    winning_cell_3 = array[2]
-    if(board[winning_cell_1] == board(winning_cell_2) && board(winning_cell_2) == board(winning_cell_3))
-      return array
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
+      return win_combination
+    else
+      # do nothing
     end
   end
   return nil
