419288caca5ee166a9262f2ce9500a00bcf59828
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-12 20:58:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,3 +15,12 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |array|
+    if (board["#{array[0]}"] == board["#{array[1]}"] && board["#{array[1]}"] == board["#{array[2]}"])
+      return array
+    end
+  end
+end
+
